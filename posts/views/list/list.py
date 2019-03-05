878c845e7cbd787b4d8419197a2f76dae7464824
bmonsalves <bdo.monsalves@gmail.com>
2019-03-05 17:02:11 -0300

    carga arquitectura

---
+++ posts/views/list/list.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from datetime import datetime
+from django.core import serializers
+
+posts = [
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/600?image=1036',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/800/800/?image=903',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://picsum.photos/500/700/?image=1076',
+    }
+]
+
+
+def list_html_posts(request):
+    return render(request, 'list/list.html', {posts: posts})
+
+
+def list_json_posts(request):
+    return JsonResponse(posts, safe=False)
